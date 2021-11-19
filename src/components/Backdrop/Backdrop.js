db0b0938aec6909cca281a4cf15d0f0389c94542
DillanR1 <dillanroby@yahoo.com>
2021-11-18 22:26:25 -0800

    background needed revision per request

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -5,12 +5,33 @@ import "../Backdrop/Backdrop.css";
 
 function Backdrop() {
   return (
-    <div className="container">
-      <img
-        className="background-image"
-        src="../images/bird-catcher.jpg"
-        alt=""
-      />
+    <div className="backdrop-container">
+      <div class="info-box" id="text-1">
+        <p class="intro-text">
+          Yo! I hear you need some sample text so this is all the sample text I
+          got. Just for you!
+        </p>
+      </div>
+
+      <div class="pic" id="pic-1">
+        <img src="../images/bird-catcher.jpg" alt="" class="backdrop-image" />
+      </div>
+
+      <div class="pic" id="pic-2">
+        <img src="../images/steam_roller.jpeg" alt="" class="backdrop-image" />
+      </div>
+
+      <div class="pic" id="pic-1">
+        <img src="../images/bird-catcher.jpg" alt="" class="backdrop-image" />
+      </div>
+
+      {/* <div class="info-box" id="text-2">
+        <p class="intro-text">
+          Yo! I hear you need some sample text so this is all the sample text I
+          got. Just for you!
+        </p>
+      </div> */}
+
       <div className="banner">
         <ul className="bullets">
           <li className="bullet">
@@ -22,9 +43,6 @@ function Backdrop() {
           </li>
         </ul>
       </div>
-      {/* <div>
-        <Img_container src="../images/hero_car.jpg" />
-      </div> */}
     </div>
   );
 }
