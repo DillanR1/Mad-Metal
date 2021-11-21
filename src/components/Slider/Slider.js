c38d3832838a4cf6262feb7ec6c34a6bdaa35636
DillanR1 <dillanroby@yahoo.com>
2021-11-21 01:28:21 -0800

    slider looks good!

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,43 +5,45 @@ import "./Slider.css";
 function Slider() {
   return (
     <>
-      <Carousel variant="dark">
-        <Carousel.Item>
-          <img
-            className="slider-images"
-            src="../images/bird-catcher.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="slider-images"
-            src="../images/bird-catcher.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="slider-images"
-            src="../images/bird-catcher.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div className="slider-container">
+        <Carousel variant="light">
+          <Carousel.Item>
+            <img
+              className="slider-images d-block w-100"
+              src="../images/bird-catcher.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="slider-images d-block w-100"
+              src="../images/bird-catcher.jpg"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="slider-images d-block w-100"
+              src="../images/bird-catcher.jpg"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h5>Third slide label</h5>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 }
