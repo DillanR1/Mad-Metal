7e0bfc831119ac9c831c2cae73e8f990d1f4583b
DillanR1 <dillanroby@yahoo.com>
2021-11-23 22:34:49 -0800

    info section added, work on marketing materials

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "../App.css";
 import Slider from "./Slider/Slider";
+import Info from "./Info/Info";
 
 function Home() {
-  return <Slider />;
+  return (
+    <>
+      <Slider />;
+      <Info />;
+    </>
+  );
 }
 
 export default Home;
---
+++ src/components/Info/Info.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Info.css";
+
+function Info() {
+  return (
+    <>
+      <div className="info-background">
+        <div className="info-text">
+          <p className="info">
+            Whether it's perfecting the tune on your weekend racer, making dad's
+            old hotrod run better than new, or finally springing for that
+            supercharger Mad Metal is here for all classic car and hotrod
+            enthusiasts. Mad Metal is bad-ass hot rodding dialed to 11.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Info;
